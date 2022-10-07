92964f9b6726a7975a2f0ed1fec0b1fa2c563e02
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-07 16:37:01 -0300

    feat: tela de login com validação do btn play

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div><LoginForm /></div>
+    );
+  }
+}
