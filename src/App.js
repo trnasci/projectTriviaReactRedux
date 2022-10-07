5de92bb5ad9df519817cf2fb16ace27db9f483ca
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-07 16:06:43 -0300

    feat: configuração e estrutura de pastas inicial do redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './trivia.png';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>SUA VEZ</p>
+        </header>
+      </div>
+    );
+  }
 }
