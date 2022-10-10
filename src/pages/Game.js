09f5acc12cba29615e501d4b9bb5f9a16374a124
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-10-10 16:14:14 -0300

    tentei começar

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,30 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Game extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    this.requestGame();
+  }
+
+  requestGame = async () => {
+    const token = localStorage.getItem('token');
+    try {
+      const result = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+      const data = await result.json();
+      this.setState({ data: data.results });
+    } catch (e) {
+      const { history } = this.props;
+      localStorage.setItem('token', '');
+      history.push('/');
+    }
+    console.log(data.results);
+  };
+
   render() {
     return (
       <div>
@@ -10,3 +33,9 @@ export default class Game extends Component {
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
