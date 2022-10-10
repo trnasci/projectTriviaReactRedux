73a50472daa2a5d93ebda276078b6d548debbaee
HiagoP <hiagoperonip@gmail.com>
2022-10-10 18:31:14 -0300

    PASSOU EM VÁRIOS VUMBORA

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 export default class Game extends Component {
   state = {
     data: [],
+    question: 0,
   };
 
   componentDidMount() {
@@ -18,17 +19,56 @@ export default class Game extends Component {
       const data = await result.json();
       this.setState({ data: data.results });
     } catch (e) {
-      const { history } = this.props;
       localStorage.setItem('token', '');
-      history.push('/');
     }
-    console.log(data.results);
+  };
+
+  shuffle = (array) => {
+    let currentIndex = array.length; let
+      randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
   };
 
   render() {
+    const { data, question } = this.state;
     return (
       <div>
         <Header />
+        {
+          data.length > 0 && (
+            <div>
+              <h2 data-testid="question-category">{data[question].category}</h2>
+              <h1 data-testid="question-text">{data[question].question}</h1>
+              <ul>
+                {
+                  this.shuffle([
+                    ...data[question].incorrect_answers, data[question].correct_answer]
+                    .map((cadaResposta, i, a) => (
+                      <li
+                        key={ cadaResposta }
+                        data-testid={
+                          `${i === a.length - 1
+                            ? 'correct' : 'wrong'}-answer${i === a.length - 1
+                            ? '' : `-${i}`}`
+                        }
+                      >
+                        <button type="button">{cadaResposta}</button>
+                      </li>)))
+                }
+              </ul>
+            </div>)
+        }
       </div>
     );
   }
