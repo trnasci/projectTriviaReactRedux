595562c4f6c1a22950f4780727a65a7dded783db
HiagoP <hiagoperonip@gmail.com>
2022-10-11 18:30:37 -0300

    feat: Requisito 12 :rocket:

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1 data-testid="feedback-text">Feedback</h1>
       </div>
     );
