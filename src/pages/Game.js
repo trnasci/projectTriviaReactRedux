115ed7fc886d0ad9a9b80a89ab69b115dc9bcc21
HiagoP <hiagoperonip@gmail.com>
2022-10-11 18:20:10 -0300

    Requisito 11 COMPLETO :rocket:

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -110,10 +110,17 @@ class Game extends Component {
 
   // botão next
   handleClickNext = () => {
+    let { question } = this.state;
+    const { history } = this.props;
+    const five = 4;
+    if (question === five) {
+      history.push('/feedback');
+    }
     this.setState((prevstate) => ({ question: prevstate.question + 1,
 
     }), () => {
-      const { question, data, answers, correctAnswer } = this.state;
+      const { data, answers, correctAnswer } = this.state;
+      ({ question } = this.state);
       const array = this.shuffle([
         ...data[question].incorrect_answers, data[question].correct_answer]);
       const wrongs = answers
